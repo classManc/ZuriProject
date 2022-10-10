15bae1bed01b27bf9e8c030832a378d3bb515f17
Ajakaye <mailpaulade@gmail.com>
2022-10-10 19:57:34 +0100

    added models

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Artiste, Song, Lyrics
 
 # Register your models here.
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyrics)
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,19 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_relaesed = models.DateTimeField()
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    like = models.IntegerField(default=0)
+
+class Lyrics(models.Model):
+    content = models.TextField()
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
